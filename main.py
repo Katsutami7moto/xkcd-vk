a1a6833175d260b9b331c6c80f71c5351f225252
Katsutami7moto <nclsf87@gmail.com>
2022-07-14 23:23:28 +0300

    Download xkcd Python comic

---
+++ main.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from urllib.parse import unquote, urlsplit, urljoin
+
+import requests
+
+
+def get_file_name_from_url(url: str) -> str:
+    return Path(unquote(urlsplit(url).path)).name
+
+
+def download_image(images_path: Path, image_url: str) -> Path:
+    img_response = requests.get(image_url)
+    img_response.raise_for_status()
+    file_path = images_path.joinpath(
+        get_file_name_from_url(image_url)
+    )
+    with open(file_path, 'wb') as file:
+        file.write(img_response.content)
+    return file_path
+
+
+def get_image_url_from_comic_url(comic_url: str):
+    response = requests.get(
+        urljoin(comic_url, 'info.0.json')
+    )
+    response.raise_for_status()
+    comic_metadata = response.json()
+    return comic_metadata['img']
+
+
+def main():
+    images_path = Path('images')
+    images_path.mkdir(parents=True, exist_ok=True)
+    comic_url = 'https://xkcd.com/353/'
+    image_url = get_image_url_from_comic_url(comic_url)
+    download_image(images_path, image_url)
+
+
+if __name__ == "__main__":
+    main()
